031abeeea660b1d6bf2d444644800dfb056dc430
chipxchip <jeremymarker@mail.weber.edu>
2026-07-06 12:07:05 -0600

    update 1

---
+++ src/components/RecipeList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./RecipeList.css";
+
+const RecipeList = (props) => {
+  return (
+    <div className="recipe-list">
+      {props.recipes.map((recipe) => (
+        <div className="recipe-card" key={recipe.id}>
+          <img
+            src={recipe.image}
+            alt={recipe.name}
+            onError={(e) => {
+              e.target.src =
+                "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=400";
+            }}
+          />
+          <div className="recipe-info">
+            <h2>{recipe.name}</h2>
+            <h3>Ingerdients</h3>
+            <p>{recipe.ingredients}</p>
+            <h3>Instructions</h3>
+            <p>{recipe.instructions}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
